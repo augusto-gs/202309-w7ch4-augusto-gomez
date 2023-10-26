ebad033012d634c47f828a4ac138cd993f38c4d2
Augusto Gomez <agomezsantos95@gmail.com>
2023-10-26 18:30:46 +0200

    Add App component

---
+++ src/components/App/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="app-container">
+      <header className="main-header">
+        <h1 className="main-header__title">Sloth Webpage</h1>
+      </header>
+    </div>
+  );
+};
+
+export default App;
